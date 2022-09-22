9ddf09e7f3cc199fbcf6e6381836bc59265d281b
Vkc23 <vip.chauhan23@gmail.com>
2022-09-22 14:04:10 +0530

    adding header and bottom

---
+++ pocket-geek-hook-ui/src/templates/bottom.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+// import { styled } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import AssurantLogo from '../../src/assets/assurant-footer-logo.png'
+import SSLLogo from '../../src/assets/ssl-logo.jpeg'
+import PCILogo from '../../src/assets/pci-logo.jpeg'
+
+
+
+export default function BottomAppBar() {
+    return (
+        <React.Fragment>
+            <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+                <Toolbar>
+                    <IconButton color="inherit" aria-label="open drawer">
+                        <img src={AssurantLogo} alt="assurant"></img>
+                    </IconButton>
+                    <Box sx={{ flexGrow: 1 }} />
+                    <IconButton color="inherit">
+                        <img src={SSLLogo} alt="SSL"></img>
+                    </IconButton>
+                    <IconButton color="inherit">
+                        <img src={PCILogo} alt="PCI"></img>
+                    </IconButton>
+                </Toolbar>
+                <Toolbar>
+                    <Typography>@ 2022 assurant inc. all right reversed</Typography>
+                    <Box sx={{ flexGrow: 1 }} />
+
+                    <Link href="#" color="inherit" style={{ margin: 5 }}>
+                        {'Support Details'}
+                    </Link>
+                    <Link href="#" color="inherit" style={{ margin: 5 }}>
+                        {'Privacy Policy'}
+                    </Link>
+                    <Link href="#" color="inherit" style={{ margin: 5 }}>
+                        {'Terms of service'}
+                    </Link>
+
+                </Toolbar>
+            </AppBar>
+        </React.Fragment>
+    );
+}
---
+++ pocket-geek-hook-ui/src/templates/fileClaim.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function FileClaim() {
+    return (<div>Main Content</div>)
+}
